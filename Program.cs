b9ef8fff43a86e779c8963e4f248340eff5fbb22
Opilane TTHK <opilane@tthk.ee>
2025-11-13 10:55:30 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimene number:");
+            float firstNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisesta teine number:");
+            float secondNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vali tehe (+, -, *, /):");
+            string input = Console.ReadLine();
+
+            switch (input)
+
+            {
+                case "+":
+                    Console.WriteLine($"tulemus: {firstNumber + secondNumber}");
+                    break;
+                //case "-":
+                //    result = firstNumber - secondNumber;
+                //    break;
+                //case "*":
+                //    result = firstNumber * secondNumber;
+                //    break;
+                //case "/":
+                //    result = firstNumber / secondNumber;
+                //    break;
+                    case "-":
+                        Console.WriteLine($"tulemus: {firstNumber - secondNumber}");
+                        break;
+                case "*":
+                    Console.WriteLine($"tulemus: {firstNumber * secondNumber}");
+                    break;
+                case "/":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("poesh govna");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"tulemus: {firstNumber / secondNumber}");
+                        
+                    }
+                    break;
+                default:
+                    Console.WriteLine("eflbjfeeflbj-");
+                    break;
+
+
+            }
+        }
+    }
+}
